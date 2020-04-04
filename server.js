aaf2adba0bb9013c42268d4e6460e60ebc713fa6
Rachel Puder <rcompuder@gmail.com>
2020-04-03 23:01:57 -0400

    updated server and css

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 // Dependencies
 const http = require("http");
 const fs = require("fs");
+var express = require('express');
+var app = express();
 
 // Set our port to 8080
 const PORT = 8080;
+app.use(express.static('client'));
+
 
 // Create a function for handling the requests and responses coming into our server
-function handleRequest(req, res) {
+// function handleRequest(req, res) {
 
-  // Here we use the fs package to read our index.html file
-  fs.readFile(__dirname + "/index.html", function(err, data) {
-    if (err) throw err;
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    // an html file.
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}
+//   // Here we use the fs package to read our index.html file
+//   fs.readFile(__dirname + "client", function(err, data) {
+//     if (err) throw err;
+//     // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+//     // an html file.
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end(data);
+//   });
+// }
 
-// Create our server
-const server = http.createServer(handleRequest);
+// // Create our server
+// const server = http.createServer(handleRequest);
 
 // Starts our server
-server.listen(PORT, function() {
+app.listen(PORT, function() {
   console.log("Server is listening on PORT: " + PORT);
 });
